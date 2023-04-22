f2c73d187ef19c0800c0392807f4bd85da413199
saito <68278331+rs0122@users.noreply.github.com>
2023-04-22 12:06:45 +0000

    Update 3 files

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./styles.css";
 
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
+<p>test</p>
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
